927ac8f6d2e0699097ff8c9bed6a8aae260dabbd
kristineatease1 <kristineatease@gmail.com>
2022-06-16 13:07:02 -0700

    shell

--- env1.c
+++ env1.c
@@ -36,14 +36,10 @@ char *_getenv(const char *name, char **_environ)
 	char *ptr_env;
 	int i, mov;
 
-	/* Initialize ptr_env value */
 	ptr_env = NULL;
 	mov = 0;
-	/* Compare all environment variables */
-	/* environ is declared in the header file */
 	for (i = 0; _environ[i]; i++)
 	{
-		/* If name and env are equal */
 		mov = cmp_env_name(_environ[i], name);
 		if (mov)
 		{
